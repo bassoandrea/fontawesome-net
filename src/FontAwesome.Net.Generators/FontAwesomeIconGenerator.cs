5a2172ae914ef9791a2f94692943c573ddada1a0
Andrea Basso <bassoandrea2201@gmail.com>
2026-02-26 10:09:40 +0100

    Implement Roslyn-based code generation for icons and styles from FontAwesome icons.js metadata

---
+++ src/FontAwesome.Net.Generators/FontAwesomeIconGenerator.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Text;
+
+namespace FontAwesome.Net.Generators;
+
+[Generator]
+public class FontAwesomeIconGenerator
+    : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        var files = context.AdditionalTextsProvider
+            .Where(at => at.Path.EndsWith("icons.json", StringComparison.CurrentCultureIgnoreCase))
+            .Select((file, ct) =>
+            {
+                var content = file.GetText(ct)?.ToString() ?? string.Empty;
+                return (path: file.Path, content: content);
+            });
+
+        context.RegisterPostInitializationOutput(spc =>
+        {
+            var source = """
+                            using System;
+                            using System.Reflection;
+                            
+                            namespace FontAwesome.Net.Generators
+                            {
+                                public partial class FontAwesomeIconStyle
+                                    : Enumeration, IFontAwesomeIconStyle
+                                {
+                                    private FontAwesomeIconStyle(int id, string name) : base(id, name)
+                                    {
+                                    }
+                                }
+                                
+                                public partial class FontAwesomeIcon
+                                    : Enumeration, IFontAwesomeIcon
+                                {
+                                    private FontAwesomeIconStyle[] _styles;
+                                
+                                    private FontAwesomeIcon(int id, string name, FontAwesomeIconStyle[] styles = null) : base(id, name)
+                                    {
+                                        _styles = styles ?? Array.Empty<FontAwesomeIconStyle>();
+                                    }
+                                    
+                                    public IFontAwesomeIconStyle[] Styles 
+                                        => _styles;
+                                    
+                                    public static FontAwesomeIcon None = new FontAwesomeIcon(0x0, nameof(None));
+                                }
+                            }
+                         """;
+
+            spc.AddSource("FontAwesome.Core.g.cs", SourceText.From(source, Encoding.UTF8));
+        });
+
+        context.RegisterSourceOutput(files, (spc, file) =>
+        {
+            var icons = IconEntries.ParseFromJson(file.content);
+            
+            var sourceStyles = icons.GenerateIconStylesCode();
+            spc.AddSource($"FontAwesome.IconStyles.g.cs", SourceText.From(sourceStyles, Encoding.UTF8));
+
+            var sourceIcons = icons.GenerateIconsCode();
+            spc.AddSource($"FontAwesome.Icons.g.cs", SourceText.From(sourceIcons, Encoding.UTF8));
+        });
+    }
+}
---
+++ src/FontAwesome.Net.Generators/IconEntries.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+
+namespace FontAwesome.Net.Generators;
+
+internal class IconEntries
+    : Dictionary<string, IconEntry>
+{
+    public static IconEntries ParseFromJson(string json)
+    {
+        return JsonSerializer.Deserialize<IconEntries>(json) ?? new IconEntries();
+    }
+
+    public string GenerateIconStylesCode()
+    {
+        var styles = this.Values
+            .SelectMany(e => e.Styles)
+            .Distinct()
+            .ToArray();
+
+        var sb = new StringBuilder();
+        sb.AppendLine("""
+                      namespace FontAwesome.Net.Generators
+                      {
+                          public partial class FontAwesomeIconStyle
+                              : Enumeration
+                          {
+                      """);
+
+        var i = 0;
+        foreach (var style in styles)
+        {
+            var name = ConvertToValidVariableName(style);
+            sb.AppendLine($"        public static FontAwesomeIconStyle {name} = new FontAwesomeIconStyle({++i}, nameof({name}));");
+        }
+        
+        sb.AppendLine("""
+                          }
+                      }
+                      """);
+        
+        return sb.ToString();
+    }
+
+    public string GenerateIconsCode()
+    {
+        var sb = new StringBuilder();
+
+        sb.AppendLine("""
+                      namespace FontAwesome.Net.Generators
+                      {
+
+                          public partial class FontAwesomeIcon
+                              : Enumeration
+                          {
+                      """);
+
+        foreach (var item in this)
+        {
+            sb.AppendLine(GenerateMember(item.Key, item.Value));
+        }
+
+        sb.AppendLine("""
+                          }
+                      }
+                      """);
+
+        return sb.ToString();
+    }
+
+    private static string GenerateMember(string iconId, IconEntry icon)
+    {
+        var name = ConvertToValidVariableName(iconId);
+        var styles = icon.Styles.Select(s => $"FontAwesomeIconStyle.{ConvertToValidVariableName(s)}").ToArray();
+        var stylesArgs = string.Join(", ", styles);
+
+        return $$"""
+                         /// <summary>
+                         /// {{icon.Label}} [<c>{{icon.Unicode}}</c>]
+                         /// <br/>Styles: <i>{{string.Join(", ", styles)}}</i>
+                         /// <para><see href="https://fontawesome.com/icons/{{iconId}}/"/></para>
+                         /// </summary>
+                         public static FontAwesomeIcon {{name}} = new FontAwesomeIcon(0x{{icon.Unicode}}, nameof({{name}}), new[] { {{stylesArgs}} });
+
+                 """;
+    }
+
+    private static string ConvertToValidVariableName(string name)
+    {
+        var textInfo = CultureInfo.InvariantCulture.TextInfo;
+        var words = Regex.Split(name, @"[^a-zA-Z0-9]+");
+
+        var result = string.Join("", words.Select(textInfo.ToTitleCase).ToArray());
+
+        if (!char.IsLetter(result.FirstOrDefault()))
+            result = $"_{result}";
+        return result;
+    }
+}
+
+internal class IconEntry
+{
+    [JsonPropertyName("label")]
+    public string Label { get; set; }
+
+    [JsonPropertyName("unicode")]
+    public string Unicode { get; set; }
+
+    [JsonPropertyName("styles")]
+    public List<string> Styles { get; set; }
+}
